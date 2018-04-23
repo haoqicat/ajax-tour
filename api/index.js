2322001980c1b45a8ce554daf25ad6e545808ff1
Peter Wang <happypeter1983@gmail.com>
2018-04-23 21:18:40 +0800

    3-api

--- api/index.js
+++ api/index.js
@@ -1,5 +1,37 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+// mongoose START
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/react-ajax')
+var db = mongoose.connection
+db.on('error', () => console.error('Mongo Failed to Connect!!!!'))
+db.on('connected', () => console.log('Mongo Connected'))
+// mongoose END
+
+app.use(bodyParser.json())
+
+const Comment = require('./models/comment.js')
+
+app.post('/comment', async (req, res) => {
+  try {
+    const comment = new Comment(req.body)
+    const cmt = await comment.save()
+    res.json({
+      comment: cmt
+    })
+  } catch (err) {
+    res.status(406).json({ msg: '提交失败' })
+  }
+})
+
+app.get('/comments', async (req, res) => {
+  const comments = await Comment.find()
+  res.json({
+    comments
+  })
+})
 
 app.get('/', (req, res) => {
   res.send('I am the API server')
