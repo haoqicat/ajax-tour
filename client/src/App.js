8b1c3f12a1012d0e53b8c56a8a2d35dcec2fb00b
Peter Wang <happypeter1983@gmail.com>
2018-04-23 21:23:36 +0800

    4-ui

---
+++ client/src/App.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+import styled, { injectGlobal } from 'styled-components'
+import poster from './poster.jpg'
+
+injectGlobal`
+  * {
+    box-sizing: border-box;
+  }
+`
+
+class App extends Component {
+  render() {
+    const comments = [
+      {
+        _id: '1',
+        text: 'hello 1'
+      },
+      {
+        _id: '2',
+        text: 'hello 2'
+      }
+    ]
+
+    const cmtList = comments.map(t => <Comment key={t._id}>{t.text}</Comment>)
+    return (
+      <Wrap>
+        <Img src={poster} />
+        <CommentList>{cmtList}</CommentList>
+        <Input />
+        <ErrMsg>提交错误</ErrMsg>
+        <Button>提交</Button>
+      </Wrap>
+    )
+  }
+}
+
+export default App
+
+const Wrap = styled.div`
+  width: 400px;
+  margin: 30px auto;
+`
+
+const Img = styled.img`
+  display: block;
+  padding: 20px;
+  width: 100%;
+  border: 2px solid #00bcd4;
+`
+
+const CommentList = styled.div`
+  margin: 20px auto;
+`
+
+const Comment = styled.div`
+  margin: 10px 0;
+  line-height: 1.9;
+`
+
+const Input = styled.textarea`
+  outline: 0;
+  border: 2px solid lavender;
+  width: 100%;
+  padding: 10px 5px;
+  &:focus {
+    outline: 0;
+    border: 2px solid #00bcd4;
+  }
+`
+
+const Button = styled.button`
+  border: 2px solid #00bcd4;
+  border-radius: 0;
+  width: 30%;
+  float: right;
+  margin: 9px 0;
+  padding: 8px;
+  &:focus {
+    outline: none;
+  }
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+const ErrMsg = styled.div`
+  width: 40%;
+  color: palevioletred;
+  margin-top: 9px;
+  float: left;
+`
---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './App'
+
+const node = document.getElementById('root')
+render(<App />, node)
